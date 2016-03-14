b4528ae727485f5dee3459b1381699143a95e6e3
NormantasKudzma <kudzma.normantas@gmail.com>
2016-03-14 20:31:27 +0200

    Created preview build 1

--- src/game/OverloadMain.java
+++ src/game/OverloadMain.java
@@ -3,11 +3,11 @@ package game;
 import engine.OverloadEngine;
 
 public class OverloadMain {	
-	public static final boolean IS_DEBUG_BUILD = true;
-	public static final String VERSION = "v0.25";
+	public static final boolean IS_DEBUG_BUILD = false;
+	public static final String VERSION = "(preview build 1)";
 	public static final String GAME = "Overload";
 	
-	public static void main(String[] args){
+	public static void main(String[] args){		
 		OverloadEngine engine = new OverloadEngine();
 		OverloadGame game = new OverloadGame();
 		engine.setGame(game);
